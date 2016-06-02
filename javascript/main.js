fce26226e148e3e98943c4d509689d8100aa8dd8
RobinZ87 <robinz87@gmail.com>
2016-06-01 22:38:52 -0400

    Rework JS
    Reduced to 1 event for testing

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,32 @@
-var main = function () {
+// var main = function () {
 
-$("h2").hover(function () {
-$(this).css("font-color","#36CFC8");
-}
-,function() {
-$(this).css("font-color","#b8b8b8");	
-};
+// $("h2").hover(function () {
+// $(this).css("font-color","#36CFC8");
+// }
+// ,function() {
+// $(this).css("font-color","#b8b8b8");	
+// };
 
-$(".nav-item").hover(function() {
-$(this).css("background-color","#36CFC8");
-}
-,function() {
-$(this).css("background-color","#b8b8b8");
-};
+// $(".nav-item").hover(function() {
+// $(this).css("background-color","#36CFC8");
+// }
+// ,function() {
+// $(this).css("background-color","#b8b8b8");
+// };
 
-$(".button").hover(function() {
-$(this).addClass("button-highlight");
-}
-,function() {
-$(this).removeClass("button-highlight");
-};
+// $(".button").hover(function() {
+// $(this).addClass("button-highlight");
+// }
+// ,function() {
+// $(this).removeClass("button-highlight");
+// };
 
-};
-$(document).ready(main);
+// };
+// $(document).ready(main);
+
+$(document).ready(function() {
+$(".button").hover() {
+$(this).toggleClass("button-highlight");
+}, function() {
+$(this).toggleClass("button-highlight");
+});
