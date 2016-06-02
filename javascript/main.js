c174000c5810395ea6eaa1bd4d2c754f723025ef
RobinZ87 <robinz87@gmail.com>
2016-06-01 22:54:51 -0400

    Js test
    bugfix

--- javascript/main.js
+++ javascript/main.js
@@ -1,35 +1,9 @@
-// var main = function () {
-
-// $("h2").hover(function () {
-// $(this).css("font-color","#36CFC8");
-// }
-// ,function() {
-// $(this).css("font-color","#b8b8b8");	
-// };
-
-// $(".nav-item").hover(function() {
-// $(this).css("background-color","#36CFC8");
-// }
-// ,function() {
-// $(this).css("background-color","#b8b8b8");
-// };
-
-// $(".button").hover(function() {
-// $(this).addClass("button-highlight");
-// }
-// ,function() {
-// $(this).removeClass("button-highlight");
-// };
-
-// };
-// $(document).ready(main);
-
 $(document).ready(function() {
 $(".button").hover(function () {
 $(this).toggleClass("button-highlight");
 }, function() {
 $(this).toggleClass("button-highlight");
-}):
+});
 });
 
 $(document).ready(function() {
